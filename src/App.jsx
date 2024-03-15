172ac515f59339afd5ec4a1f7fd90beb0df78887
ashrafur rahman farhad <itsfaarhaad@gmail.com>
2024-03-15 03:43:00 +0000

    removing from cart function added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { RiDeleteBack2Fill } from "react-icons/ri";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -16,13 +17,18 @@ function App() {
   // console.log(products);
 
   const handleCart = p => {
-    const isExist = cart.find(product => product.id == p.id);
+    const isExist = cart.find(item => item.id == p.id);
     if (!isExist) {
       setCart([...cart, p]);
     }
     else {
       alert('Already in cart');
     }
+  }
+
+  const removeFromCart = id => {
+    const newCart = cart.filter(item => item.id !== id);
+    setCart(newCart);
   }
 
   return (
@@ -37,8 +43,30 @@ function App() {
             ></SingleProduct>)
           }
         </div>
-        <div className="w-1/3 shadow-md cart-container ml-4">
+        <div className="w-1/3 shadow-md cart-container ml-4 p-10">
           <h2 className="text-3xl p-4 border-b-2">This is Cart</h2>
+          <div className="flex justify-between p-2 border-b">
+            <h4>Name</h4>
+            <h4>Price</h4>
+          </div>
+          <div>
+            {
+              cart.map((item) => (
+                <>
+
+                  <div className="flex justify-between items-center p-2 w-full">
+                    <h4>{item.title.slice(0, 10)}</h4>
+                    <div className="flex items-center justify-center">
+                      <h4>{item.price} $</h4>&#160;&#160;
+                      <button onClick={() => removeFromCart(item.id)} className="text-red-700"><RiDeleteBack2Fill />
+                      </button>
+                    </div>
+
+                  </div>
+                </>
+              ))
+            }
+          </div>
         </div>
       </div>
     </>
